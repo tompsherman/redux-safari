d55c8ac97b5d9ccd349fab950ad9f2bd2a488ee5
unknown <tompsherman@gmail.com>
2020-10-15 21:10:09 -0400

    login scaffold

--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import axiosWithAuth from '../utils/axiosWithAuth'
 
-export default function Login(props) {
+
+const Login = (props) => {
     // How can we log in? What do we need to do?
+
+    const [login, setLogin] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    const handleChange = (event) => {
+        
+    }
+
 
     return (
         <div>
-            <h1>Welcome to the Safari App!</h1>
-            <h2>I can't show you more until you log in. Please build out a login.</h2>
+            <h1 className="adventure">Lets go Safari</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                name="username" 
+                type="text" 
+                value={props.username} 
+                onChange={handleChange} 
+                placeholder="enter username" 
+                />
+
+                <input 
+                name="password" 
+                type="password" 
+                value={props.password} 
+                onChange={handleChange} 
+                placeholder="enter password" 
+                />
+                <button>See that Tiger!</button>
+            </form>
         </div>
     )
 }
+
+export default Login
